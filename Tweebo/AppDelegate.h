7c80b50289fc52666f31dc8fc1f72a2b44aa8291
Jonny Zen <Jonny_Zen@live.com>
2016-08-08 16:54:27 +0800

    Initial Commit

---
+++ Tweebo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tweebo
+//
+//  Created by 苍曜石 on 16/8/8.
+//  Copyright © 2016年 Zen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
